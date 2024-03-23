8f4f1fdf70cdb46523d3f5e5fa66f1abb6db3717
JasonOCat <jason.catbagan@gmail.com>
2024-03-23 05:53:28 +0100

    Use streaming with suspense on LatestInvoices component

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -39,6 +39,9 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   noStore();
   try {
+    console.log('Fetching latest Invoices...');
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
