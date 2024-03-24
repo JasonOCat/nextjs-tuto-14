3338a8240aa8c617c1643310d3c6d2e5e3c2691e
JasonOCat <jason.catbagan@gmail.com>
2024-03-24 06:33:31 +0100

    Create create invoice form with a Server Action

---
+++ app/lib/actions.ts
@@ -0,0 +1,14 @@
+'use server';
+
+export default async function createInvoice(formData: FormData) {
+  const rawFormData = {
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  };
+  // If many fields, can use : const rawFormData = Object.fromEntries(formData.entries())
+
+  console.log(rawFormData);
+  console.log(typeof rawFormData.amount);
+
+};
