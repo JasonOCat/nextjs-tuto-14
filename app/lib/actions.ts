a465219ed6ed3e89b07d7ff763e35598fdbf6276
JasonOCat <jason.catbagan@gmail.com>
2024-03-24 06:38:16 +0100

    Use lib Zod to handle type validation and coercion (change)

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,14 +1,29 @@
 'use server';
 
+import { z } from 'zod';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
 export default async function createInvoice(formData: FormData) {
-  const rawFormData = {
+
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  };
+  });
   // If many fields, can use : const rawFormData = Object.fromEntries(formData.entries())
 
-  console.log(rawFormData);
-  console.log(typeof rawFormData.amount);
+  const amountInCents = amount * 100;
+
+  console.log({customerId, amount, status});
+  console.log(typeof amount);
 
 };
