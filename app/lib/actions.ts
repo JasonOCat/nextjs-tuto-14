8565560ccff35f4fea594b4a01f406034cb9354a
JasonOCat <jason.catbagan@gmail.com>
2024-03-24 23:47:18 +0100

    Update invoice form validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -75,15 +75,23 @@ export  async function createInvoice(prevState : State, formData: FormData) {
 }
 
 export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
 
-  const amountInCents = amount * 100;
+  if (!validatedFields.success) {
 
-  console.log(status)
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
+  const amountInCents = amount * 100;
 
   try {
     await sql`
